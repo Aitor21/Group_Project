0bf9556773433b4c77266b1e1fa1cdb501ea243c
Aitor21 <aitormorales94@gmail.com>
2013-12-10 05:44:13 -0500

    Added Buttons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -5,6 +5,28 @@ define([], function () {
     $(document).ready(function(){
     alert("Bienvenido");
     })
+    $("#blue").hover(function(){
+    	  $("#blue").css("background-color","#207ce5");
+    	  },function(){
+    	  $("#blue").css("background-color","blue");
+    	});
+    $("#red").hover(function(){
+  	  $("#red").css("background-color","#e73827");
+  	  },function(){
+  	  $("#red").css("background-color","red");
+  	});
+    $("#green").hover(function(){
+  	  $("#green").css("background-color","#91e842");
+  	  },function(){
+  	  $("#green").css("background-color","green");
+  	});
+    $("#yellow").hover(function(){
+  	  $("#yellow").css("background-color","#f2fa05");
+  	  },function(){
+  	  $("#yellow").css("background-color","yellow");
+  	});
 
     return '\'Allo \'Allo!';
 });
+
+
