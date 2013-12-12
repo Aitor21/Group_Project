35cf61bca99ecf71c9b00fa84843642a7e9ca136
Aitor21 <aitormorales94@gmail.com>
2013-12-12 06:37:32 -0500

    Buttons click

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,37 @@ define([], function () {
     
     $(document).ready(function(){
     alert("Bienvenido");
-    })
-    $("#blue").hover(function(){
+    
+    var highlight = function(button, color){
+    	var oldColor = button.css("background-color")
+    	button.css("background-color", color);
+    	setTimeout(function(){
+    		button.css("background-color", oldColor)
+    	}, 200)
+    }
+ 
+    var userClicks = new Array()
+    $(".button").click(function(){
+  		var thisId = $(this).attr('id')
+  			if(thisId === "blue"){
+  				userClicks.push(thisId);
+  				highlight($(this), "yellow")
+  			}else if (thisId === 'red'){
+  				userClicks.push(thisId);
+  				highlight($(this), "#a00")
+  			}else if (thisId=== "green"){
+  				userClicks.push(thisId);
+  				highlight($(this), "#a00")
+  		  	}else if (thisId === "yellow"){
+  		  		userClicks.push(thisId);
+  		  		highlight($(this), "#a00")
+  		  	}else{
+  		  		console.log("No funciona")
+  		  	}
+  		console.log(userClicks);
+    }) 
+    
+   /* $("#blue").hover(function(){
     	  $("#blue").css("background-color","#207ce5");
     	  },function(){
     	  $("#blue").css("background-color","blue");
@@ -24,9 +53,8 @@ define([], function () {
   	  $("#yellow").css("background-color","#f2fa05");
   	  },function(){
   	  $("#yellow").css("background-color","yellow");
-  	});
-
+  	  });	*/
+  		
     return '\'Allo \'Allo!';
-});
-
-
+})
+})
