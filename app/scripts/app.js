3f68bec6687d0b27e6596f7449be0a4a66ea4a74
Aitor21 <aitormorales94@gmail.com>
2013-12-16 05:47:43 -0500

    Button Change

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,61 @@ define([], function () {
     
     $(document).ready(function(){
     alert("Bienvenido");
+    var highlight = function(button, color) {
+        var oldColor = button.css("background-color")
+        button.css("background-color", color)
+        setTimeout(function() {
+           button.css("background-color", oldColor)
+        }, 200)
+      }
+      
+      var userClicks = new Array()
+      var buttonList
+      var computerSequence = new Array()
+      
+      $(document).ready(function() {
+          buttonList = jQuery.map( $(".button"),
+                         function(element) {
+                           return $(element).attr('id')
+                         })
+          $(".button").click( function(){
+               computerSequence.push( buttonList[
+                                        Math.floor(Math.random()
+                                          * buttonList.length)] )
+               console.log(computerSequence)
+               var thisId = $(this).attr('id')
+               if(thisId === buttonList[0]) {
+                 userClicks.push(thisId)
+                 highlight($(this), "yellow")
+               } else if ( thisId === buttonList[1]) {
+                 userClicks.push(thisId)
+                 highlight($(this), "#aa0")
+               } else if ( thisId === buttonList[2]) {
+                 userClicks.push(thisId)
+                 highlight($(this), "#00a")
+               } else if ( thisId === buttonList[3]) {
+                 userClicks.push(thisId)
+                 highlight($(this), "#0a0")
+               } else {
+                 console.log("Wrong button ID detected")
+               }
+               //console.log(userClicks)
+          })
+      })
+   /* var buttonList;
+    var computerSequence = new Array();
     
+    buttonList = jQuery.map( $(".button"),
+    		function(element){
+    	return $(element).attr('id');
+    })
+    $(".button").click(function(){
+    	computerSequence.push(buttonList[
+    	                                 Math.floor(Math.random()
+    	                                		 *buttonList.length)])
+    }
+    console.log(computerSequence)
+    var thisId = $ (this)attr
     var highlight = function(button, color){
     	var oldColor = button.css("background-color")
     	button.css("background-color", color);
@@ -18,21 +72,21 @@ define([], function () {
   		var thisId = $(this).attr('id')
   			if(thisId === "blue"){
   				userClicks.push(thisId);
-  				highlight($(this), "yellow")
+  				highlight($(this), "red")
   			}else if (thisId === 'red'){
   				userClicks.push(thisId);
-  				highlight($(this), "#a00")
+  				highlight($(this), "blue")
   			}else if (thisId=== "green"){
   				userClicks.push(thisId);
-  				highlight($(this), "#a00")
+  				highlight($(this), "yellow")
   		  	}else if (thisId === "yellow"){
   		  		userClicks.push(thisId);
-  		  		highlight($(this), "#a00")
+  		  		highlight($(this), "green")
   		  	}else{
   		  		console.log("No funciona")
   		  	}
   		console.log(userClicks);
-    }) 
+    }) */
     
    /* $("#blue").hover(function(){
     	  $("#blue").css("background-color","#207ce5");
@@ -57,4 +111,13 @@ define([], function () {
   		
     return '\'Allo \'Allo!';
 })
+})
+
+
+
+var buttonList;
+
+buttonList = jQuery.map( $(".button"),
+		function(element){
+	return $(element).attr('id');
 })
